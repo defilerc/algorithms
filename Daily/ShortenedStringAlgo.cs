0c323afbf82ff7b47a23ee113689dfbae1ceedea
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2017-12-29 10:54:41 +0200

    add shortened string daily

---
+++ Daily/ShortenedStringAlgo.cs
@@ -0,0 +1,34 @@
+﻿namespace Algo
+{
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class ShortenedStringAlgo
+    {
+        public string shortenedString(string ss)
+        {
+            ss = Regex.Replace(ss, @"([A-Z])(\d+)",
+                m => new StringBuilder().Insert(0, m.Groups[1].Value,
+                    int.Parse(m.Groups[2].Value)).ToString());
+            
+            ss = Regex.Replace(ss, @"\(([A-Z\s]+)\)(\d+)",
+                    m => new StringBuilder().Insert(0, m.Groups[1].Value,
+                        int.Parse(m.Groups[2].Value)).ToString());
+
+            return Regex.Match(ss, "[0-9]+").Success ? shortenedString(ss) : Regex.Replace(ss, @"\(|\)|\s", "");
+        }
+
+        string shortenedStringShort(string s) => s == (s = Regex.Replace(
+                                                s, @" |([A-Z]|\((\w+)\))(\d+)", 
+                                                m => string.Concat(
+                                                    Enumerable.Repeat(
+                                                        m.Groups[m.Groups[2] + s != s ? 2 : 1] + "",
+                                                        int.Parse("0" + m.Groups[3])
+                                                    )
+                                                )
+                                            ))
+            ? s
+            : shortenedString(s);
+    }
+}
---
+++ DailyTests/ShortenedStringTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTests
+{
+    using Algo;
+
+    [TestClass]
+    public class ShortenedStringTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var algo = new ShortenedStringAlgo();
+            var result = algo.shortenedString("(X3Y2(Z1)3 M4B0)2H2O");
+            result = algo.shortenedString("A3");
+            result = algo.shortenedString("(AB)3 B5");
+        }
+    }
+}
