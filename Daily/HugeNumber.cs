289bf60ab3f597c14698820222224371c580a91a
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2017-12-29 10:58:23 +0200

    add refined hugenumbers (still not working)

--- Daily/HugeNumber.cs
+++ Daily/HugeNumber.cs
@@ -1,13 +1,22 @@
-﻿using System;
-
-namespace Daily
+﻿namespace Daily
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     public class HugeNumber
     {
         public string hugeNumber(string[] nums)
         {
-            Array.Sort(nums, (a, b) => b.CompareTo(a));
-            return string.Concat(nums);
+            var maxLength = nums.Max(n => n.Length);
+            var nump = new List<string>();
+
+            foreach (var num in nums) {
+                nump.Add(num.PadRight(maxLength, 'x'));
+            }
+
+            nump = nump.OrderByDescending(n => n).ToList();
+            //Array.Sort(nump, (a, b) => b.CompareTo(a));
+            return string.Concat(nump).Replace("x", "");
         }
     }
 }
--- DailyTests/HugeNumberTests.cs
+++ DailyTests/HugeNumberTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Daily;
+﻿using Daily;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DailyTests
@@ -11,7 +10,12 @@ namespace DailyTests
         public void TestMethod1()
         {
             var algo = new HugeNumber();
-            var result = algo.hugeNumber(new [] {"20", "3005", "2"});
+
+            var result = algo.hugeNumber(new [] {"286", "973", "572", "907", "193", "929", "93", "552", "842", "789", 
+                "910", "11", "906", "998", "298", "12", "78", "294", "999", "24", "117"});
+            Assert.AreEqual("9999989739392991090790684278978572552298294286241931211711", result);
+
+            result = algo.hugeNumber(new [] {"20", "3005", "2"});
             Assert.AreEqual("3005220", result);
         }
     }
