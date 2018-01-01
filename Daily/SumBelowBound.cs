144b6b5a80a11ffd02c8485782546b0cb9bf2027
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2018-01-01 10:54:17 +0200

    add sum below bound

---
+++ Daily/SumBelowBound.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Daily
+{
+    public class SumBelowBound
+    {
+        public int sumBelowBound(int b)
+        {
+            var n = 1;
+            while ((b -= n++) >= 0) ;
+            return n;
+        }
+
+        public int sumBelowBoundShort2(int b) => (int) (Math.Sqrt(1 + 8 * b)-1) / 2;
+
+    }
+}
