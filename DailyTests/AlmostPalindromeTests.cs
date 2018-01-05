921c7413b5cb6e2d9c0966d6c52c7c7404f6c797
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2018-01-05 21:59:35 +0200

    add almost palindrome

---
+++ DailyTests/AlmostPalindromeTests.cs
@@ -0,0 +1,24 @@
+﻿using Daily;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DailyTests
+{
+    [TestClass]
+    public class AlmostPalindromeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var algo = new AlmostPalindrome();
+            var result = false;
+            result = algo.almostPalindrome("babababa");
+            result = algo.almostPalindrome("acac");
+            result = algo.almostPalindrome("abcd");
+            result = algo.almostPalindrome("abcbcdf");
+            result = algo.almostPalindrome("bba");
+            result = algo.almostPalindrome("abcbcdf");
+            result = algo.almostPalindrome("abcbcdf");
+            Assert.AreEqual(false, result);
+        }
+    }
+}
