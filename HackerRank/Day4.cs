6ca54dc8488ee3c189c433656cd402630d681a3f
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2018-02-05 06:51:27 +0200

    hr day4

---
+++ HackerRank/Day4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Person
+{
+    int age;
+
+    public Person(int initialAge)
+    {
+
+        if (initialAge < 0) {
+            Console.WriteLine("Age is not valid, setting age to 0.");
+        }
+
+        this.age = initialAge;
+    }
+}
