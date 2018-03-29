48d0a8fcc93a0667ff4ee8da65045b97619a9033
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2018-03-29 22:20:04 +0300

    add disjoint sets

---
+++ HackerEarth/Disjoint/DisjointAlgo.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace HackerEarth.Disjoint
+{
+    public class DisjointAlgo
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+        public static int[] Init(int length)
+        {
+            return Enumerable.Range(0, length + 1).ToArray();
+        }
+
+        public static bool Find(int[] array, int a, int b)
+        {
+            return a < array.Length && b < array.Length && array[a] == array[b];
+        }
+
+        /// <summary>
+        /// Connect array[a] with array[b], by making array[a] = array[b]
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public static void Union(int[] array, int a, int b)
+        {
+            var tmp = array[a];
+            for (var i = 0; i < array.Length; i++) {
+                if (array[i] == tmp) {
+                    array[i] = array[b];
+                }
+            }
+        }
+    }
+}
---
+++ HackerEarthTests/Disjoint/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using HackerEarth.Disjoint;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerEarthTests.Disjoint
+{
+    [TestClass]
+    public class DisjointAlgoTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a = Enumerable.Range(0, 10).ToArray();
+
+            DisjointAlgo.Union(a, 2, 1);
+            DisjointAlgo.Union(a, 4, 3);
+            DisjointAlgo.Union(a, 8, 4);
+            DisjointAlgo.Union(a, 9, 3);
+            DisjointAlgo.Union(a, 6, 5);
+        }
+    }
+}
