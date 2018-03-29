c2e8c542682810fd5b88536c9f19632249c49eb5
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2018-03-29 21:59:25 +0300

    add tests

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,13 +1,24 @@
-﻿using HackerEarth.MarCircuits;
+﻿using HackerRank;
+using HackerEarth.MarCircuits;
+using HackerEarth.Graphs;
+using System.Collections.Generic;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        //MonkNewArray.Main(new string[0]);
-        //MonkAndNumberSol.Main(new string[0]);
-        //TreeDiameter.Main(new string[0]);
-        //BreakingEdgesAlgo.Main(new string[0]);
-        MissileBombingAlgo.Main(new string[0]);
+        UnreachableNodesAlgo.Main(new string[0]);
     }
+}
+
+class Solution
+{
+    private Stack<char> s = new Stack<char>();
+    private Queue<char> q = new Queue<char>();
+
+    void pushCharacter(char ch) => s.Push(ch);
+    char popCharacter(char ch) => s.Pop();
+
+    void enqueueCharacter(char ch) => q.Enqueue(ch);
+    char dequeueCharacter(char ch) => q.Dequeue();
 }
---
+++ HackerEarthTests/MarCircuits/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using HackerEarth.MarCircuits;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerEarthTests.MarCircuits
+{
+    [TestClass]
+    public class BreakingEdgesTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            BreakingEdgesAlgo.Main(new string[0]);
+        }
+    }
+}
