d46693b16e1c565f8e200425b1052f0f8713fb06
Konstantinos Athanasoglou <defilerc@gmail.com>
2018-04-08 22:08:20 +0300

    add lisnlogn

---
+++ HackerEarthTests/CodeMonk/Checkpoint2/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using HackerEarth.CodeMonk.Checkpoint2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerEarthTests.CodeMonk.Checkpoint2
+{
+    [TestClass]
+    public class LISNLogNTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var A = new[] { 2, 5, 3, 7, 11, 8, 10, 13, 6 };
+
+            var result = LISNLogN.LongestIncreasingSubsequenceLength(A, A.Length);
+        }
+    }
+}
