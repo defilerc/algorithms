da6239e0ec55ff9f40b0894fba9c9b27469b52c0
Konstantinos Athanasoglou <defilerc@gmail.com>
2018-04-08 21:48:58 +0300

    add huge number

--- Daily/HugeNumber.cs
+++ Daily/HugeNumber.cs
@@ -1,5 +1,6 @@
 ﻿namespace Daily
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
@@ -7,16 +8,49 @@
     {
         public string hugeNumber(string[] nums)
         {
-            var maxLength = nums.Max(n => n.Length);
-            var nump = new List<string>();
+            Array.Sort(nums, (a, b) => b.CompareTo(a));
 
-            foreach (var num in nums) {
-                nump.Add(num.PadRight(maxLength, 'x'));
+            var nump = new List<string>();
+            nump = nump.OrderByDescending(n => n).ToList();
+
+            for (var i = 0; i < nums.Length; i++) {
+                if (i == nums.Length - 1) {
+                    nump.Add(nums[i]);
+                }
+
+                if (nums[i].Length == nums[i + 1].Length) {
+                    nump.Add(nums[i]);
+                }
             }
 
-            nump = nump.OrderByDescending(n => n).ToList();
-            //Array.Sort(nump, (a, b) => b.CompareTo(a));
-            return string.Concat(nump).Replace("x", "");
+            return string.Concat(nump);
+        }
+
+        public string hugeNumberRec(string[] nums)
+        {
+            Array.Sort(nums, (a, b) => b.CompareTo(a));
+            var nump = new List<string>();
+
+            Compare(nums, 0, 1, ref nump);
+
+            return string.Concat(nump);
+        }
+
+        private void Compare(string[] nums, int i1, int i2,
+            ref List<string> nump)
+        {
+            if (i1 >= nums.Length) {
+                return;
+            }
+
+            if (i2 >= nums.Length) {
+                nump.Add(nums[i1]);
+            }
+
+            if (nums[i1].Length == nums[i2].Length) {
+                nump.Add(nums[i1]);
+                Compare(nums, i1+1, i2+1, ref nump);
+            }
         }
     }
 }
